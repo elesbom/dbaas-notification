b96cb40258a880bf0936436d42a06dba9443f79c
Elesbom <rafael.goncalves@corp.globo.com>
2017-07-19 11:09:08 -0300

    Get config from environment

--- dbaas_notification/app.py
+++ dbaas_notification/app.py
@@ -1,14 +1,13 @@
+import os
 from flask import Flask
-# from flask_redis import FlaskRedis
 from dbaas_notification.router import add_url_rules
 from flask_redis import Redis
 
 flask_app = Flask("dbaas_notification")
-flask_app.config['REDIS_URL'] = 'redis://:EMSgrxGPby@dbaasnotif-01-150040581922.dev.redis.globoi.com:6379/0'
-flask_app.config['REDIS_HOST'] = 'dbaasnotif-01-150040581922.dev.redis.globoi.com'
-flask_app.config['REDIS_PORT'] = 6379
-flask_app.config['REDIS_DB'] = 0
-flask_app.config['REDIS_PASSWORD'] = 'EMSgrxGPby'
+flask_app.config['REDIS_HOST'] = os.getenv('REDIS_HOST')
+flask_app.config['REDIS_PORT'] = os.getenv('REDIS_PORT', 6379)
+flask_app.config['REDIS_DB'] = os.getenv('REDIS_DB', 0)
+flask_app.config['REDIS_PASSWORD'] = os.getenv('REDIS_PASSWORD')
 add_url_rules(flask_app)
 redis_store = Redis(flask_app)
 
